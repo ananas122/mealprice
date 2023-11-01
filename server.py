882ca5e72304b0888487c17665ffaa934c018a42
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-11-01 16:08:06 +0100

    ajout des requetes selected recipes

--- server.py
+++ server.py
@@ -1,30 +1,50 @@
-
-from flask import Flask, render_template, redirect, request, jsonify
+import uuid
+from werkzeug.security import check_password_hash
+from flask import Flask, render_template, redirect, request, jsonify, make_response, session
 from flask_restful import Api, Resource
 from flask_mysqldb import MySQL
 from datetime import datetime
 from dateutil.parser import parse
 from flask_cors import CORS
-
-# Charger les variables d'environnement
-from dotenv import load_dotenv
-load_dotenv()
+from flask_session import Session
+import json
+import logging
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_USE_SIGNER'] = True
+app.config['SECRET_KEY'] = 'ananasma'
+Session(app)
 
-# Configuration de la base de données MySQL en utilisant les variables d'environnement
 app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
-# Initialisez l'extension Flask-MySQL
 mysql = MySQL(app)
+
+
+@app.route('/set_cookie')
+def set_cookie():
+    session_id = str(uuid.uuid4())
+    response = make_response("Setting a cookie")
+    response.set_cookie(app.config["SESSION_COOKIE_NAME"], str(session_id))
+    return response
+
+
+@app.route('/clearcookie')
+def clear_cookie():
+    resp = make_response("Cookie Cleared")
+    resp.set_cookie("my_session_cookie", expires=0)
+    return resp
 
 
 @app.route('/add_ingredient', methods=['POST'])
@@ -36,7 +56,7 @@ def add_ingredient():
     unit_cost = request.json.get('unit_cost')
     stock_quantity = request.json.get('stock_quantity')
 
-    cur.execute("INSERT INTO ingredients (name, category, supplier, unit_cost, stock_quantity) VALUES (%s, %s, %s, %s, %s, %s)",
+    cur.execute("INSERT INTO ingredients (name, category, supplier, unit_cost, stock_quantity) VALUES (%s, %s, %s, %s, %s)",
                 (name, category, supplier, unit_cost, stock_quantity))
     mysql.connection.commit()
     cur.close()
@@ -44,31 +64,57 @@ def add_ingredient():
     return 'Ingrédient ajouté avec succès', 201
 
 
-# Routes pour différentes pages
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/', methods=['GET', 'POST'])
+def main_page():
+    if request.method == 'POST':
+        form_name = request.form.get('form_name')
+
+        if form_name == 'signup':
+            try:
+                # ... code to handle signup requests ...
+                return 'Utilisateur enregistré avec succès', 201
+            except Exception as e:
+                print(f"Erreur lors de l'enregistrement de l'utilisateur : {e}")
+                return 'Erreur lors de l\'enregistrement', 500
+
+        elif form_name == 'login':
+            try:
+                # ... code to handle login requests ...
+                return 'Utilisateur connecté avec succès', 200
+            except Exception as e:
+                print(f"Erreur lors de la connexion de l'utilisateur : {e}")
+                return 'Erreur lors de la connexion', 500
+
+    return render_template('main_page.html')
+
+
+def get_recipe_from_db(recipe_id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM recettes WHERE id=%s", [recipe_id])
+    recipe = cur.fetchone()
+    cur.close()
+    return recipe
+
 
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
 
+
 @app.route('/index1')
 def index1():
     return render_template('index1.html')
-    
-    
+
+
 @app.route('/recettes')
 def recettes():
     return render_template('recettes.html')
+
 
 @app.route('/calendar')
 def calendar():
     return render_template('calendar.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
 
 @app.route('/stocks')
 def stocks():
@@ -79,41 +125,50 @@ def stocks():
 def ingredients():
     return render_template('ingredients.html')
 
-    
+
 @app.route('/edit_ingredient', methods=['POST'])
 def edit_ingredient():
     ingredient_id = request.form.get('ingredient_id')
-
-    # Redirigez l'utilisateur vers la page des ingrédients après l'édition
     return redirect('/ingredients')
 
 
-@app.route('/calculator')
-def calculator():
-    return render_template('calculator.html')
-@app.route('/api/recipes', methods=['GET'])
-def fetch_recipes():
-    query = request.args.get('query')
-    api_key = '6820604bd3a44f849de0112f112d822e'
-    url = f'https://api.spoonacular.com/recipes/complexSearch?apiKey={api_key}&query={query}'
+@app.route('/set_selected_recipe', methods=['POST'])
+def set_selected_recipe():
+    selected_recipe = request.get_json()
 
-    try:
-        response = requests.get(url)
-        data = response.json()
-        return jsonify(data)
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+    if selected_recipe is None:
+        return jsonify({'message': 'Invalid JSON'}), 400
+
+    session['selected_recipe'] = json.dumps(selected_recipe, ensure_ascii=False)
+    return jsonify({'message': 'Recipe details stored in session'})
+
+
+@app.route("/calculator", methods=["GET", "POST"])
+def calculator():
+    recipe = {
+        "name": "",
+        "ingredients": [],
+        "instructions": "",
+        "quantity_used": ""
+    }
+
+    if 'selected_recipe' in session:
+        recipe_json_string = session.get('selected_recipe')
+        recipe_data = json.loads(recipe_json_string)
+        recipe['name'] = recipe_data.get('name')
+        recipe['ingredients'] = recipe_data.get('ingredients', [])
+        recipe['instructions'] = recipe_data.get('instructions', "")
+        recipe['quantity_used'] = recipe_data.get('quantity_used', "")
+
+    return render_template("calculator.html", recipe=recipe)
+
 
 @app.route('/get_recipe/<int:recipe_id>', methods=['GET'])
 def get_recipe(recipe_id):
-    # Code pour faire la requête à l'API Spoonacular ici
-    # Assurez-vous de gérer les réponses de l'API correctement
-    # Ensuite, renvoyez les données de la recette sous forme de JSON
     recipe_data = {
         'recipe_id': recipe_id,
         'recipe_name': 'Nom de la recette',
         'ingredients': ['Ingrédient 1', 'Ingrédient 2', 'Ingrédient 3'],
-        # Autres données de recette
     }
     return jsonify(recipe_data)
 
@@ -125,15 +180,27 @@ def get_recipe_details(recipe_id):
     recipe = cur.fetchone()
     cur.close()
 
-    # Convertir la recette en format JSON et la retourner
     return jsonify(recipe)
 
 
-# calendar
-events = []  # Liste pour stocker des événements de calendrier
+@app.route('/select_recipe', methods=['POST'])
+def select_recipe():
+    data = request.json
+
+    session['selectedRecipe'] = {
+        'name': data['name'],
+        'ingredients': data['ingredients'],
+        'instructions': data['instructions']
+    }
+
+    return jsonify(success=True)
+
+
+events = []
 class Event(Resource):
     def get(self, event_id):
-        if event := events_collection.find_one({'id': event_id}):
+        event = next((event for event in events if event['id'] == event_id), None)
+        if event:
             return event, 200
         else:
             return "Événement non trouvé", 404
@@ -149,12 +216,11 @@ api.add_resource(Event, "/event", "/event/<string:event_id>")
 @app.route('/get_events', methods=['GET'])
 def get_events():
     cur = mysql.connection.cursor()
-    # Adaptez cette requête à votre structure de table
     cur.execute("SELECT * FROM events")
-    events = cur.fetchall()
+    db_events = cur.fetchall()
     cur.close()
 
-    return jsonify(events), 200
+    return jsonify(db_events), 200
 
 
 @app.route('/save_event', methods=['POST'])
@@ -163,16 +229,14 @@ def save_event():
     event_data = request.json
     title = event_data.get('title')
     start_datetime = event_data.get('start_datetime')
-    print(f"Title: {title}, Start Time: {start_datetime}")  # Debug log
+    print(f"Title: {title}, Start Time: {start_datetime}")
 
-    # Convertir la date en un objet datetime
     try:
         datetime_obj = parse(start_datetime)
     except Exception as e:
-        print(f"Erreur lors de la conversion de la date: {e}")  # Debug log
+        print(f"Erreur lors de la conversion de la date: {e}")
         return 'Format de date invalide', 400
 
-    # Convertir en format MySQL
     mysql_format = datetime_obj.strftime('%Y-%m-%d %H:%M:%S')
 
     try:
@@ -180,14 +244,12 @@ def save_event():
                     (title, mysql_format))
         mysql.connection.commit()
     except Exception as e:
-        # Debug log
         print(f"Erreur lors de l'insertion dans la base de données: {e}")
         return "Erreur lors de la sauvegarde de l'événement", 500
 
     cur.close()
 
     return 'Événement sauvegardé', 201
-
 
 
 @app.route('/get_product_image', methods=['GET'])
@@ -201,10 +263,6 @@ def get_product_image():
         return "Produit non trouvé ou image indisponible", 404
 
     return product_data['image_url'], 200
-
-
-
-
 
 
 if __name__ == '__main__':
