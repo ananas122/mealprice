99b19cf63de8d12c06e0b57b1d3e68b851997353
ananas122 <122387449+ananas122@users.noreply.github.com>
2023-11-04 11:09:49 +0100

    to delete mysql = MySQL (application)

--- server.py
+++ server.py
@@ -29,8 +29,8 @@ app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
-mysql = MySQL(app)
-
+# depuis flask_mysqldb importe MySQL
+# mysql = MySQL (application)
 
 @app.route('/set_cookie')
 def set_cookie():
